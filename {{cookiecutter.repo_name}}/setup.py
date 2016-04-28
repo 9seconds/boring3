88470c2a27a4804da828a5b684f1b82c2d113767
9seconds <nineseconds@yandex.ru>
2016-04-28 10:41:13 +0300

    Remove tox command from setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -4,33 +4,6 @@
 
 import setuptools
 
-from setuptools.command.test import test as TestCommand  # NOQA
-import sys
-
-
-class Tox(TestCommand):
-
-    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.tox_args = None
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import shlex
-        import tox
-
-        args = self.tox_args
-        if args:
-            args = shlex.split(self.tox_args)
-        errno = tox.cmdline(args=args)
-        sys.exit(errno)
-
 {% if cookiecutter.with_pbr == "yes" %}
 try:
     import multiprocessing
@@ -41,8 +14,6 @@ except ImportError:
 
 setuptools.setup(
     setup_requires=["pbr>=1.8"],
-    tests_require=["tox"],
-    cmdclass={'test': Tox},
     pbr=True)
 {% else %}
 def get_description():
@@ -88,7 +59,5 @@ setuptools.setup(
         "{{ cookiecutter.package }}"
     ],
     install_requires=requires,
-    dependency_links=links,
-    tests_require=["tox"],
-    cmdclass={'test': Tox})
+    dependency_links=links)
 {% endif -%}
