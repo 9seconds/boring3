b2008572106564210e35381ef3c9fe81988a3887
9seconds <nineseconds@yandex.ru>
2019-06-24 09:11:45 +0300

    setuptools_scm integration

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -4,7 +4,11 @@
 
 import setuptools
 
-setuptools.setup(
-    install_requires=[
-    ]
-)
+
+# please check following link to understand how everything works:
+# https://setuptools.readthedocs.io/en/latest/setuptools.html#configuring-setup-using-setup-cfg-files
+{%- if cookiecutter.setuptools_scm == "no" %}
+setuptools.setup()
+{%- else %}
+setuptools.setup(use_scm_version=True)
+{%- endif %}
